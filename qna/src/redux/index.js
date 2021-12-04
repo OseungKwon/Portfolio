d9393a4d3150b853b7da89490ea681fa9760cd89
OseungKwon <00osos@naver.com>
2021-12-05 01:46:55 +0900

    필요없는 폴더 정리

--- qna/src/redux/index.js
+++
@@ -1,8 +0,0 @@
-// import { configureStore } from "@reduxjs/toolkit";
-import { combineReducers } from "redux";
-import commentReducer from "./comment";
-import qnaReducer from "./qna";
-export default combineReducers({
-  comment: commentReducer,
-  qna: qnaReducer,
-});
