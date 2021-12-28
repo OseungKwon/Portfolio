a508fdf58d1fd5602594f089d5ec4a2aac35af11
OseungKwon <00osos@naver.com>
2021-12-28 17:41:55 +0900

    useRef 에러 해결
     DOM을 다룰 때 반드시 초깃값은 null로 설정해야 한다.
    https://darrengwon.tistory.com/865

--- src/Hook/useScrollFadeIn.tsx
+++ src/Hook/useScrollFadeIn.tsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect, useCallback } from "react";
 
 const useScrollFadeIn = (direction = "up", duration = 1, delay = 0) => {
-  const element = useRef();
+  const element = useRef<HTMLDivElement>(null);
 
   const handleDirection = (name: string) => {
     switch (name) {
@@ -22,19 +22,21 @@ const useScrollFadeIn = (direction = "up", duration = 1, delay = 0) => {
     ([entry]) => {
       const { current } = element;
       if (entry.isIntersecting) {
-        current.style.transitionProperty = "all";
-        current.style.transitionDuration = `${duration}s`;
-        current.style.transitionTimingFunction = "cubic-bezier(0, 0, 0.2, 1)";
-        current.style.transitionDelay = `${delay}s`;
-        current.style.opacity = 1;
-        current.style.transform = "translate3d(0, 0, 0)";
+        if (current != null) {
+          current.style.transitionProperty = "all";
+          current.style.transitionDuration = `${duration}s`;
+          current.style.transitionTimingFunction = "cubic-bezier(0, 0, 0.2, 1)";
+          current.style.transitionDelay = `${delay}s`;
+          current.style.opacity = "1";
+          current.style.transform = "translate3d(0, 0, 0)";
+        }
       }
     },
     [delay, duration]
   );
 
   useEffect(() => {
-    let observer;
+    let observer: any;
 
     if (element.current) {
       observer = new IntersectionObserver(onScroll, { threshold: 0.7 });
